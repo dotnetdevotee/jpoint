5cbc3e6cb4eaf2f6bb8a78c492f3bc4edd5a70f2
Derek Ferguson <derekmferguson@yahoo.co.uk>
2018-02-03 08:33:14 -0600

    Registering microservices with Eureka

--- v1/services/accounts/src/main/java/ru/jug/SpringBootAccountsMicroservice.java
+++ v1/services/accounts/src/main/java/ru/jug/SpringBootAccountsMicroservice.java
@@ -2,8 +2,10 @@ package ru.jug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootAccountsMicroservice {
 
 	public static void main(String[] args) {
--- v1/services/shopping/src/main/java/ru/jug/SpringBootStoresMicroservice.java
+++ v1/services/shopping/src/main/java/ru/jug/SpringBootStoresMicroservice.java
@@ -2,12 +2,13 @@ package ru.jug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootStoresMicroservice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		SpringApplication.run(SpringBootStoresMicroservice.class, args);
 	}
 
