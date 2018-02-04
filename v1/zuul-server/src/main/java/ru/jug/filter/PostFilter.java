6565b963fb6b6e7821c0da021487c8859a8cf1c9
Derek Ferguson <derekmferguson@yahoo.co.uk>
2018-02-04 00:29:54 -0600

    Looks up reporting after each call

--- v1/zuul-server/src/main/java/ru/jug/filter/PostFilter.java
+++ v1/zuul-server/src/main/java/ru/jug/filter/PostFilter.java
@@ -1,9 +1,27 @@
 package ru.jug.filter;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.SimpleRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.discovery.DiscoveryClientRouteLocator;
+
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 
+@EnableConfigurationProperties( { ZuulProperties.class } )
 public class PostFilter extends ZuulFilter {
-
+	
+	@Autowired
+	private ServerProperties server;
+	
+	@Autowired
+	private ZuulProperties zuulProperties;
+	
 	@Override
 	public String filterType() {
 		return "post";
@@ -21,7 +39,23 @@ public class PostFilter extends ZuulFilter {
 
 	@Override
 	public Object run() {
-		System.out.println("Using Post Filter");
+		
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+	    //String path = request.getServletPath();
+		String path = "/reporting";
+
+	    SimpleRouteLocator routeLocator = 
+	    		new SimpleRouteLocator(this.server.getServletPrefix(), zuulProperties);
+	    
+	    Route route = routeLocator.getMatchingRoute(path);
+	    String serviceID = route.getLocation(); 
+		
+	    System.out.println("si: " + serviceID);
+		
+		System.out.println(
+				"Request Method : " + request.getMethod() + " Request URL : " 
+				+ request.getRequestURL().toString()); 
 
 		return null;
 	}
